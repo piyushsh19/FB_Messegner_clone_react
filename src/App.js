940f5858255cd5a28c3afa5da1998ede0e967f1c
piyush-sh19 <piyush.sh19@gmail.com>
2020-07-21 03:14:05 +0530

    logo

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
 
   return (
     <div className="App">
-      <img src=""></img>
+      <img src="https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100" alt="logo" />
       <h1>Messanger App</h1>
       <h2>Welcome {username}</h2>
       <form className="app__form">
